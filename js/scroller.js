5b99db70cba562bb9fe4789f1d239bad4929ced9
jentino <jentino@gmail.com>
2017-09-03 21:24:30 +0200

    added () to if -> syntax error

--- js/scroller.js
+++ js/scroller.js
@@ -1,6 +1,6 @@
 function myStopFunction() {
 
-		if document.body.scrollTop <= 0 {
+		if (document.body.scrollTop <= 0) {
 			console.log("scrolling down");
 		} else {
 			console.log("scrolling up");
